f16910d6dc8fd896cb5fcfa3dedace4639227cb3
Aleksandr Zelenskiy <al.a.zelenskiy@gmail.com>
2026-01-08 21:38:22 +0800

    Fix build app error #4

--- src/app/utils/regions.ts
+++ src/app/utils/regions.ts
@@ -1,5 +1,6 @@
-export { REGIONS as RUSSIAN_REGIONS, REGION_MAP, REGION_ISO_MAP } from '@/app/constants/regions';
+import { REGIONS as RUSSIAN_REGIONS, REGION_MAP, REGION_ISO_MAP } from '@/app/constants/regions';
 import type { RegionOption as RegionOptionConst } from '@/app/constants/regions';
+export { RUSSIAN_REGIONS, REGION_MAP, REGION_ISO_MAP };
 export type RegionOption = RegionOptionConst;
 export type RegionCode = RegionOption['code'];
 
