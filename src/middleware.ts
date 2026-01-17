06046ba9243dd948c8eaa39c10437c6fc030d1af
Aleksandr Zelenskiy <al.a.zelenskiy@gmail.com>
2026-01-17 23:58:45 +0800

    Fix build app error #3

--- src/middleware.ts
+++ src/middleware.ts
@@ -52,7 +52,8 @@ export default clerkMiddleware(async (auth, request) => {
   if (isAllowedGuestReportApi(request)) return NextResponse.next();
 
   // Защищаем всё остальное
-  return auth.protect();
+  await auth.protect();
+  return NextResponse.next();
 });
 
 export const config = {
